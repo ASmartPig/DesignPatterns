0515600f85d4e7d2f7385f7807be5c8c62d0954e
liaoze <616655260@qq.com>
2019-05-07 17:08:05 +0800

    提交忽略文件

---
+++ src/main/java/com/smart/Interceptors/ExceptionResolver.java
@@ -0,0 +1,19 @@
+package com.smart.Interceptors;
+
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author liaoze
+ * @Description
+ * @Author 2019/5/7 上午10:19
+ **/
+public class ExceptionResolver implements HandlerExceptionResolver {
+    @Override
+    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        return null;
+    }
+}
